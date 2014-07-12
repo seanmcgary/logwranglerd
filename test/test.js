565588edec6b9dac46f35f0db2814f99fdf6e703
Sean McGary <sean@seanmcgary.com>
2014-07-11 21:38:52 -0700

    adding elasticsearch connection

--- test/test.js
+++ test/test.js
@@ -1,13 +1,19 @@
 var interfaces = require('../lib/interfaces');
 
 var instances = interfaces.init({
+	elasticsearch: {
+		host: 'elasticsearch.local',
+		port: 9200
+	},
 	interfaces: {
 		http: {
 			port: 9999,
 			auth: function(payload, cb){
-				console.log("AUTH");
-				console.log(payload.get());
-				console.log(payload.constructor);
+				//console.log("AUTH");
+				//console.log(payload.get());
+				//console.log(payload.constructor);
+
+				cb();
 			}
 		}
 	}
